505378797b71eee289b4996b94ebdbf7ed15cad9
Sebastian Bassi <sbassi@genesdigitales.com>
2015-05-24 02:52:08 -0300

    working state

--- index.py
+++ index.py
@@ -1,22 +1,48 @@
-from bottle import route, run, template
+from bottle import post, run, template, request
+import sqlite3
+import settings
 
-@route('/surveypost/<token>')
-def index(name):
+SURVEY_DB_FILE = settings.SURVEY_DB_FILE
+
+
+
+@post('/surveypost/<token>')
+def index(token):
+
+    conn = sqlite3.connect(SURVEY_DB_FILE)
+    c = conn.cursor()
     # check that token exists
 
-    if _authenticate(token):
-        pass
+    def _authenticate(token):
+        c.execute("SELECT * FROM users WHERE token = ?", (token,))
+        return c.fetchone()
+
+    def _submited(token):
+        c.execute("SELECT * FROM result WHERE token = ?", (token,))
+        return c.fetchone()
+
+    if _authenticate(token) and not _submited(token):
+        #print token
+        q1 = request.forms.get('delivery')
+        q2 = request.forms.get('feeling')
+        comment = request.forms.get('comment')
+        c.execute("INSERT INTO result VALUES (?,?,?,?)", 
+                 (token,q1,q2,comment))
+        conn.commit()
+        conn.close()
+    elif _submited(token):
+        conn.close()
+        return 'Survey already submited'
+
     else:
         raise error
 
 
 
-    return template('Thank you!')
+    #return template('Thank you!')
+    return 'Thank you!'
 
 
-def _authenticate(token):
-    
-    return true
 
 
 
