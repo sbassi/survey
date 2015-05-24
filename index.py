7af859dbb0eafc1e1634c7c5e3d76ed5b3011b73
Sebastian Bassi <sbassi@genesdigitales.com>
2015-05-23 23:31:25 -0300

    index back

---
+++ index.py
@@ -0,0 +1,25 @@
+from bottle import route, run, template
+
+@route('/surveypost/<token>')
+def index(name):
+    # check that token exists
+
+    if _authenticate(token):
+        pass
+    else:
+        raise error
+
+
+
+    return template('Thank you!')
+
+
+def _authenticate(token):
+    
+    return true
+
+
+
+
+
+run(host='localhost', port=8080)
